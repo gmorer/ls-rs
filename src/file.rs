3cfd963df9695d98e52ba8964c117d1651aed0a7
tet <gmorer@student.42.fr>
2017-11-21 00:55:13 +0100

    error handling, block size and nlink

--- src/file.rs
+++ src/file.rs
@@ -1,94 +1,106 @@
 use std::os::unix::fs::PermissionsExt;
-use std::os::linux::fs::MetadataExt;
-use std::time::SystemTime;
+use std::os::unix::fs::MetadataExt;
+use std::ffi::OsStr;
 use std::fmt;
 use std;
 
+#[derive(Debug)]
 pub struct File {
-name: String,
-		  permissions: String,
-		  size: u64,
-		  uid: u32,
-		  gid: u32,
-		  modified: SystemTime
+    name: String,
+    permissions: String,
+    block: u64,
+    nlink: u64,
+    size: u64,
+    uid: u32,
+    gid: u32,
+    modified: i64,
 }
 
 impl File {
-	pub fn new(file: std::fs::DirEntry) -> File
-	{
-		let mut permissions = String::from("");
-		let mut bit = 256;
+    pub fn new(file: std::fs::DirEntry) -> File {
+        let mut permissions = String::from("");
+        let mut bit = 256;
 
-		if let Ok(data) = file.metadata() {
-			if data.file_type().is_dir() {
-				permissions.push('d');
-			} else if data.file_type().is_symlink() {
-				permissions.push('l');
-			} else if data.file_type().is_file() {
-				permissions.push('-');
-			} else {
-				permissions.push('?');
-			}
-			loop {
-				if (bit == 256 || bit == 32 || bit == 4) && data.permissions().mode()
-					& bit != 0 {
-						permissions.push('r');
-					} else if (bit == 128 || bit == 16 || bit == 2) && data.permissions().mode()
-						& bit != 0 {
-							permissions.push('w');
-						} else if (bit == 64 || bit == 8 || bit == 1) && data.permissions().mode()
-							& bit != 0 {
-								permissions.push('x');
-							} else {
-								permissions.push('-');
-							}
-						if bit == 1 {
-							break;
-						}
-						bit = bit / 2;
-			}
-			return File {
-name: match file.path().file_name() {
-		  None => "????????".to_string(),
-			   Some(name) => match name.to_str() {
-				   None => "????????".to_string(),
-				   Some(name) => name.to_string()
-			   }
-	  },
-permissions: permissions,
-			 size: data.len(),
-			 uid: data.st_uid(),
-			 gid: data.st_gid(),
-			 modified: SystemTime::now()
-			}
-		}
-		else {
-			println!("Couldn't read metadata for {}", file.path().display());
-			permissions.push_str("?????????");
-			return File {
-name: match file.path().file_name() {
-		  None => "????????".to_string(),
-			   Some(name) => match name.to_str() {
-				   None => "????????".to_string(),
-				   Some(name) => name.to_string()
-			   }
-	  },
-permissions: permissions,
-			 size: 0,
-			 uid: 0,
-			 gid: 0,
-			 modified: SystemTime::now()
-			}
-		}
-	}
-	pub fn cmp(&self, f: &File) -> std::cmp::Ordering {
-		self.name.cmp(&f.name)
-	}
+        if let Ok(data) = file.metadata() {
+            if data.file_type().is_dir() {
+                permissions.push('d');
+            } else if data.file_type().is_symlink() {
+                permissions.push('l');
+            } else if data.file_type().is_file() {
+                permissions.push('-');
+            } else {
+                permissions.push('?');
+            }
+            loop {
+                if (bit == 256 || bit == 32 || bit == 4) && data.permissions().mode() & bit != 0 {
+                    permissions.push('r');
+                } else if (bit == 128 || bit == 16 || bit == 2) &&
+                           data.permissions().mode() & bit != 0
+                {
+                    permissions.push('w');
+                } else if (bit == 64 || bit == 8 || bit == 1) &&
+                           data.permissions().mode() & bit != 0
+                {
+                    permissions.push('x');
+                } else {
+                    permissions.push('-');
+                }
+                if bit == 1 {
+                    break;
+                }
+                bit = bit / 2;
+            }
+            return File {
+                name: file.path()
+                    .file_name()
+                    .unwrap_or(OsStr::new("??????????"))
+                    .to_string_lossy()
+                    .into_owned(),
+                permissions: permissions,
+                block: data.blocks(),
+                nlink: data.nlink(),
+                size: data.len(),
+                uid: data.uid(),
+                gid: data.gid(),
+                modified: data.mtime(),
+            };
+        } else {
+            println!("Couldn't read metadata for {}", file.path().display());
+            permissions.push_str("?????????");
+            return File {
+                name: file.path()
+                    .file_name()
+                    .unwrap_or(OsStr::new("??????????"))
+                    .to_string_lossy()
+                    .into_owned(),
+                permissions: permissions,
+                nlink: 0,
+                block: 0,
+                size: 0,
+                uid: 0,
+                gid: 0,
+                modified: 0,
+            };
+        }
+    }
+    pub fn cmp(&self, f: &File) -> std::cmp::Ordering {
+        self.name.cmp(&f.name)
+    }
 }
 
+
 impl fmt::Display for File {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{} {} {} {} time {}", self.permissions, self.uid, self.gid, self.size,
-				/*self.modified,*/ self.name)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} {} {} {} {} {} {}",
+            self.permissions,
+            self.nlink,
+            self.uid,
+            self.gid,
+            self.size,
+            self.modified,
+            self.name
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,31 @@ pub mod file;
 
 fn main() {
 
-	let mut paths: Vec<PathBuf> = args().skip(1).map(From::from).collect();
+    let mut paths: Vec<PathBuf> = args().skip(1).map(From::from).collect();
 
-	paths.sort();
+    paths.sort();
 
-	for path in paths {
-		match std::fs::read_dir(path.as_path()){
-			Ok(dir) => {
-				let mut files: Vec<file::File> = std::vec::Vec::new();
-				println!("{}:", path.display());
-				for file in dir {
-					if let Ok(file) = file {
-						files.push(file::File::new(file));
-					}
-				}
-				//sort
-				files.sort_by(|a, b| a.cmp(b));
-				for file in files {
-					println!("{}", file);
-				}
-			}
-			Err(err) => {
-				println!("ls-rs: cannot access '{:?}': {}", path, err.description());
-			}
-		}
-	}
+    for path in paths {
+        match std::fs::read_dir(path.as_path()) {
+            Ok(dir) => {
+                let mut files: Vec<file::File> = std::vec::Vec::new();
+                println!("{}:", path.display());
+                for file in dir {
+                    if let Ok(file) = file {
+                        files.push(file::File::new(file));
+                    }
+                }
+                //sort
+                files.sort_by(|a, b| a.cmp(b));
+                for file in files {
+                    //println!("{:#?}", file);
+                    println!("{}", file);
+                }
+            }
+            Err(err) => {
+                println!("ls-rs: cannot access '{:?}': {}", path, err.description());
+
+            }
+        }
+    }
 }
