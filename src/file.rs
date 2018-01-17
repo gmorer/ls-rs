9f5de4aa9151c87a03b453552c7f101744b07092
tet <gmorer@student.42.fr>
2018-01-17 22:42:50 +0100

    large print ok

--- src/file.rs
+++ src/file.rs
@@ -1,14 +1,11 @@
 extern crate time;
 extern crate libc;
 
-//pub mod options;
-
 use option;
 
 use std::os::unix::fs::PermissionsExt;
 use std::os::unix::fs::MetadataExt;
 use std::ffi::CStr;
-use std::fmt;
 use std::mem;
 use std::ptr;
 use std;
@@ -23,94 +20,96 @@ pub struct File {
     owner: String,
     group: String,
     modified: i64,
-    time : String
+    time: String,
 }
 
 fn read_permission(data: &std::fs::Metadata) -> String {
-	let mut permissions = String::from("");
+    let mut permissions = String::from("");
     let mut bit = 256;
 
-        if data.file_type().is_dir() {
-            permissions.push('d');
-        } else if data.file_type().is_symlink() {
-            permissions.push('l');
-        } else if data.file_type().is_file() {
-            permissions.push('-');
+    if data.file_type().is_dir() {
+        permissions.push('d');
+    } else if data.file_type().is_symlink() {
+        permissions.push('l');
+    } else if data.file_type().is_file() {
+        permissions.push('-');
+    } else {
+        permissions.push('?');
+    }
+    loop {
+        if (bit == 256 || bit == 32 || bit == 4) && data.permissions().mode() & bit != 0 {
+            permissions.push('r');
+        } else if (bit == 128 || bit == 16 || bit == 2) && data.permissions().mode() & bit != 0 {
+            permissions.push('w');
+        } else if (bit == 64 || bit == 8 || bit == 1) && data.permissions().mode() & bit != 0 {
+            permissions.push('x');
         } else {
-           permissions.push('?');
+            permissions.push('-');
         }
-        loop {
-            if (bit == 256 || bit == 32 || bit == 4) &&
-				data.permissions().mode() & bit != 0 {
-                permissions.push('r');
-            } else if (bit == 128 || bit == 16 || bit == 2) &&
-                       data.permissions().mode() & bit != 0
-            {
-                permissions.push('w');
-            } else if (bit == 64 || bit == 8 || bit == 1) &&
-                       data.permissions().mode() & bit != 0
-            {
-                permissions.push('x');
-            } else {
-                permissions.push('-');
-            }
-            if bit == 1 {
-                break;
-            }
-            bit = bit / 2;
+        if bit == 1 {
+            break;
         }
-		permissions
+        bit = bit / 2;
+    }
+    permissions
 }
 
-fn get_groupename(gid :u32) -> String {
+fn get_groupename(gid: u32) -> String {
     // futur optimistation static Mutex<HasMap<u32, String>>
-	unsafe{
-		let mut rslt = ptr::null_mut();
-		let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
-			n if n < 0 => 512 as usize,
-			n => n as usize,
-		};
-		let mut buf = Vec::with_capacity(amt);
-		let mut passwd: libc::group = mem::zeroed();
-		match libc::getgrgid_r(gid, &mut passwd, buf.as_mut_ptr(),
-			buf.capacity() as libc::size_t, &mut rslt) {
-			0 if !rslt.is_null() => {
-				CStr::from_ptr(passwd.gr_name).to_str().unwrap().to_owned()
-			}
-			_=> "????".to_string()
-		}
-	}
+    unsafe {
+        let mut rslt = ptr::null_mut();
+        let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
+            n if n < 0 => 512 as usize,
+            n => n as usize,
+        };
+        let mut buf = Vec::with_capacity(amt);
+        let mut passwd: libc::group = mem::zeroed();
+        match libc::getgrgid_r(
+            gid,
+            &mut passwd,
+            buf.as_mut_ptr(),
+            buf.capacity() as libc::size_t,
+            &mut rslt,
+        ) {
+            0 if !rslt.is_null() => CStr::from_ptr(passwd.gr_name).to_str().unwrap().to_owned(),
+            _ => "????".to_string(),
+        }
+    }
 }
 
-fn get_username(uid :u32) -> String {
+fn get_username(uid: u32) -> String {
     // futur optimistation static Mutex<HasMap<u32, String>>
-	unsafe{
-		let mut rslt = ptr::null_mut();
-		let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
-			n if n < 0 => 512 as usize,
-			n => n as usize,
-		};
-		let mut buf = Vec::with_capacity(amt);
-		let mut passwd: libc::passwd = mem::zeroed();
-		match libc::getpwuid_r(uid, &mut passwd, buf.as_mut_ptr(),
-			buf.capacity() as libc::size_t, &mut rslt) {
-			0 if !rslt.is_null() => {
-				CStr::from_ptr(passwd.pw_name).to_str().unwrap().to_owned()
-			}
-			_=> "????".to_string()
-		}
-	}
+    unsafe {
+        let mut rslt = ptr::null_mut();
+        let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
+            n if n < 0 => 512 as usize,
+            n => n as usize,
+        };
+        let mut buf = Vec::with_capacity(amt);
+        let mut passwd: libc::passwd = mem::zeroed();
+        match libc::getpwuid_r(
+            uid,
+            &mut passwd,
+            buf.as_mut_ptr(),
+            buf.capacity() as libc::size_t,
+            &mut rslt,
+        ) {
+            0 if !rslt.is_null() => CStr::from_ptr(passwd.pw_name).to_str().unwrap().to_owned(),
+            _ => "????".to_string(),
+        }
+    }
 }
 
 impl File {
     pub fn new(file: std::fs::DirEntry) -> File {
         if let Ok(data) = file.metadata() {
             return File {
-                name: file.file_name()
-                    .to_string_lossy()
-                    .into_owned(),
+                name: file.file_name().to_string_lossy().into_owned(),
                 permissions: read_permission(&data),
-                time: time::strftime("%b %d %R", &time::at_utc(time::Timespec::new(data.mtime(), 0))).unwrap(),
+                time: time::strftime(
+                    "%b %d %R",
+                    &time::at_utc(time::Timespec::new(data.mtime(), 0)),
+                ).unwrap(),
                 block: data.blocks(),
                 nlink: data.nlink(),
                 size: data.len(),
@@ -121,9 +120,7 @@ impl File {
         } else {
             eprintln!("Couldn't read metadata for {}", file.path().display());
             return File {
-                name: file.file_name()
-                    .to_string_lossy()
-                    .into_owned(),
+                name: file.file_name().to_string_lossy().into_owned(),
                 permissions: "??????????".to_string(),
                 nlink: 0,
                 time: "?????????????".to_string(),
@@ -135,38 +132,55 @@ impl File {
             };
         }
     }
-	pub fn is_directory(&self) -> bool {self.permissions.starts_with("d")}
-	pub fn name(&self) -> &String {&self.name}
-    pub fn cmp(&self, f: &File, options: u8) -> std::cmp::Ordering {
-		let mut rslt: std::cmp::Ordering = std::cmp::Ordering::Equal;
-		if option::option_t(options)  {
-			rslt = f.modified.cmp(&self.modified);
-		} else if option::option_ss(options)  {
-			rslt = f.size.cmp(&self.size);
-		}
-		if rslt == std::cmp::Ordering::Equal {
-        	rslt = self.name.cmp(&f.name);
-		}
-		if option::option_r(options) {
-			return rslt.reverse()
-		}
-		rslt
+    pub fn is_directory(&self) -> bool {
+        self.permissions.starts_with("d")
     }
-}
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+    pub fn nlink(&self) -> &u64 {
+        &self.nlink
+    }
+    pub fn owner(&self) -> &String {
+        &self.owner
+    }
+    pub fn group(&self) -> &String {
+        &self.group
+    }
+    pub fn size(&self) -> &u64 {
+        &self.size
+    }
+    pub fn print(&self, max: (usize, usize, usize, usize)) -> () {
+        println!(
+            "{perm} {nlink:<nlink_max$} {owner:<owner_max$} {group:<group_max$} \
+            {size:<size_max$} {time} {name}",
+            perm = &self.permissions,
+            nlink = &self.nlink,
+            owner = &self.owner,
+            group = &self.group,
+            size = &self.size,
+            time = &self.time,
+            name = &self.name,
+            nlink_max = max.0,
+            owner_max = max.1,
+            group_max = max.2,
+            size_max = max.3
+        );
+    }
 
-
-impl fmt::Display for File {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {} {} {} {} {}",
-            self.permissions,
-            self.nlink,
-            self.owner,
-            self.group,
-            self.size,
-            self.time,
-            self.name
-        )
+    pub fn cmp(&self, f: &File, options: u8) -> std::cmp::Ordering {
+        let mut rslt: std::cmp::Ordering = std::cmp::Ordering::Equal;
+        if option::option_t(options) {
+            rslt = f.modified.cmp(&self.modified);
+        } else if option::option_ss(options) {
+            rslt = f.size.cmp(&self.size);
+        }
+        if rslt == std::cmp::Ordering::Equal {
+            rslt = self.name.cmp(&f.name);
+        }
+        if option::option_r(options) {
+            return rslt.reverse();
+        }
+        rslt
     }
 }
--- src/print.rs
+++ src/print.rs
@@ -7,39 +7,68 @@ use file;
 use std;
 use option;
 
-#[cfg(target_os="macos")]
-const TIOCGWINSZ: c_ulong  = 0x40087468;
-#[cfg(all(target_env="musl", not(target_os="macos")))]
+#[cfg(target_os = "macos")]
+const TIOCGWINSZ: c_ulong = 0x40087468;
+#[cfg(all(target_env = "musl", not(target_os = "macos")))]
 const TIOCGWINSZ: c_int = 0x00005413;
-#[cfg(all(not(target_env="musl"), not(target_os="macos")))]
+#[cfg(all(not(target_env = "musl"), not(target_os = "macos")))]
 const TIOCGWINSZ: c_ulong = 0x00005413;
 
-//#[warn(dead_code)]
 struct WinSize {
     _ws_row: c_ushort,
     ws_col: c_ushort,
     _ws_xpixel: c_ushort,
-    _ws_ypixel: c_ushort
+    _ws_ypixel: c_ushort,
 }
 
 fn get_term_size() -> Option<usize> {
-    if unsafe{libc::isatty(libc::STDOUT_FILENO) != 1} {
-        return None
+    if unsafe { libc::isatty(libc::STDOUT_FILENO) != 1 } {
+        return None;
     }
-    Some ( unsafe {
-        let mut winsize = WinSize{_ws_row: 0, ws_col: 0, _ws_xpixel: 0, _ws_ypixel: 0};
-        libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &mut winsize);
-        let cols = if winsize.ws_col > 0 { winsize.ws_col } else { 0 };
+    Some(unsafe {
+        let mut winsize = WinSize {
+            _ws_row: 0,
+            ws_col: 0,
+            _ws_xpixel: 0,
+            _ws_ypixel: 0,
+        };
+        libc::ioctl(libc::STDOUT_FILENO, TIOCGWINSZ, &mut winsize);
+        let cols = if winsize.ws_col > 0 {
+            winsize.ws_col
+        } else {
+            0
+        };
         cols as usize
     })
 }
 
-fn max_size(files: &Vec<file::File>) -> usize {
+fn max_size_big(files: &Vec<file::File>) -> (usize, usize, usize, usize) {
+    let mut max: (usize, usize, usize, usize) = (0, 0, 0, 0);
+    /* tuple: (nlink, uname, gname, size) */
+
+    for file in files {
+        if file.nlink().to_string().chars().count() > max.0 {
+            max.0 = file.nlink().to_string().chars().count();
+        }
+        if file.owner().chars().count() > max.1 {
+            max.1 = file.owner().chars().count();
+        }
+        if file.group().chars().count() > max.2 {
+            max.2 = file.group().chars().count();
+        }
+        if file.size().to_string().chars().count() > max.3 {
+            max.3 = file.size().to_string().chars().count();
+        }
+    }
+    max
+}
+
+fn max_size_litle(files: &Vec<file::File>) -> usize {
     let mut max: usize = 0;
 
     for file in files {
-        if file.name().len() > max {
-            max = file.name().len();
+        if file.name().chars().count() > max {
+            max = file.name().chars().count();
         }
     }
     max
@@ -48,7 +77,8 @@ fn max_size(files: &Vec<file::File>) -> usize {
 fn print_small(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<PathBuf> {
     lazy_static!{static ref TERM_SIZE: Option<usize> = get_term_size(); }
     let mut new_paths = std::vec::Vec::new();
-    let max_size = max_size(&files);
+    let max_size = max_size_litle(&files); //TODO get the max size with the file creation
+
     for (index, file) in files.iter().enumerate() {
         if option::option_rr(options) && file.is_directory() {
             let mut new_path = old_path.clone();
@@ -57,14 +87,14 @@ fn print_small(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<Pa
         }
         match *TERM_SIZE {
             Some(size) => {
-                let col = size / (max_size + 10);
-                if (index + 1) % col == 0 {
+                let col = size / (max_size + 4);
+                if col == 0 || (index + 1) % col == 0 {
                     println!("{}", file.name());
                 } else {
-                    print!("{:<lenght$}    ", file.name(), lenght=max_size);
+                    print!("{:<lenght$}    ", file.name(), lenght = max_size);
                 }
-            },
-            None => println!("{}", file.name())
+            }
+            None => println!("{}", file.name()),
         }
     }
     println!("");
@@ -73,6 +103,7 @@ fn print_small(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<Pa
 
 fn print_big(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<PathBuf> {
     let mut new_paths = std::vec::Vec::new();
+    let max = max_size_big(&files);
 
     for file in files {
         if option::option_rr(options) && file.is_directory() {
@@ -80,7 +111,7 @@ fn print_big(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<Path
             new_path.push(file.name());
             new_paths.push(new_path);
         }
-        println!("{}", file);
+        file.print(max);
 
     }
     new_paths
@@ -89,9 +120,12 @@ fn print_big(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<Path
 pub fn print_file(files: Vec<file::File>, old_path: PathBuf, options: u8) -> Vec<PathBuf> {
 
     if option::option_l(options) {
-        print_big(files, old_path, options)
+        let ret = print_big(files, old_path, options);
+        println!("");
+        ret
     } else {
-        print_small(files, old_path, options)
+        let ret = print_small(files, old_path, options);
+        println!("");
+        ret
     }
-    //println!("");
 }
